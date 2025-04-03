9ccd1d280b4ec78422201df863cdd269caa5d10f
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2025-04-03 19:33:26 -0400

    Initial commit

---
+++ src/main/java/com/all_books/Book.java
@@ -0,0 +1,11 @@
+package com.all_books;
+
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private String isbn;
+    private int quantity;
+
+}
